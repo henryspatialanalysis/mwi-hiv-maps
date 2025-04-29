319672e5c39e2c67f2c66bd0b4cab155d81cb693
Nathaniel Henry <nat.henry7@gmail.com>
2025-04-28 20:00:28 -0700

    Add legend and color scheme for each area.

--- assets/js/build_district_maps.js
+++ assets/js/build_district_maps.js
@@ -1,7 +1,54 @@
+// DUMMY: These will be read from a data file in the future
+const viz_options = {};
+const default_viz_options = {
+  prevalence: {
+    pop_cutoff_low: 50,
+    pop_cutoff_high: 150,
+    use_col: 'prev15to49_mean',
+    lower: 0.0,
+    upper: 0.15,
+    fill_palette: [
+      '#0D0887','#3E049C','#6300A7','#8707A6','#A62098','#C03A83','#D5546E',
+      '#E76F5A','#F58C46','#FDAD32','#FCD225','#F0F921'
+    ],
+    legend_breaks: ['0%', '5%', '10%', '15%+'],
+    legend_title: 'Estimated<br/>HIV Prevalence'
+  },
+  viraemia: {
+    pop_cutoff_low: 50,
+    pop_cutoff_high: 150,
+    use_col: 'viraemia15to49_mean',
+    lower: 0.0,
+    upper: 0.10,
+    legend_breaks: ['0%', '5%', '10%'],
+    fill_palette: [
+      '#30123B','#4454C4','#4490FE','#1FC8DE','#29EFA2','#7DFF56','#C1F334',
+      '#F1CA3A','#FE922A'
+    ],
+    legend_title: 'Estimated<br/>HIV Viraemia',
+  },
+  vls: {
+    pop_cutoff_low: 50,
+    pop_cutoff_high: 150,
+    use_col: 'vls15to49_mean',
+    lower: 0.7,
+    upper: 1.0,
+    legend_breaks: ['<=70%', '80%', '90%', '100%'],
+    fill_palette: [
+      '#440154','#482878','#3E4A89','#31688E','#26828E','#1F9E89','#35B779',
+      '#6DCD59','#B4DE2C'
+    ],
+    legend_title: 'Estimated<br/>Viral Load<br/>Suppression'
+  }
+}
+
 // Script used to build district-specific maps
-create_map('hiv-viraemia-map', boundaries);
-create_map('hiv-prevalence-map', boundaries);
-create_map('viral-load-suppression-map', boundaries);
+const viraemia_options = {...default_viz_options.viraemia, ...viz_options.viraemia};
+create_map('hiv-viraemia-map', boundaries, viraemia_options);
+const prevalence_options = {...default_viz_options.prevalence, ...viz_options.prevalence};
+create_map('hiv-prevalence-map', boundaries, prevalence_options);
+const vls_options = {...default_viz_options.vls, ...viz_options.vls};
+create_map('viral-load-suppression-map', boundaries, vls_options);
 
 // Fill tables with placeholders
 document.getElementById('traditional-authorities-table').innerHTML = '<strong>PLACEHOLDER</strong>';
--- assets/js/leaflet_functions.js
+++ assets/js/leaflet_functions.js
@@ -10,8 +10,8 @@ function pct(outcome, acc = 0.1, suffix = true) {
   return (outcome * 100).toFixed(acc) + suffixMark;
 }
 
-
 // Create labels for map areas ---------------------------------------------------------->
+
 function poly_tooltop(layer, ind_suffix = ''){
   const props = layer.feature.properties;
   const cols = Object.keys(props);
@@ -70,30 +70,178 @@ function poly_tooltop(layer, ind_suffix = ''){
   return inner_html;
 }
 
+
+// Function to add a legend to the map -------------------------------------------------->
+
+function prepare_legend(options){
+  const default_options = {
+    legend_title: null,
+    className: 'leaflet-legend',
+    legend_breaks: ['Lowest', 'Highest'],
+    fill_palette: [
+      '#0D0887FF','#3E049CFF','#6300A7FF','#8707A6FF','#A62098FF','#C03A83FF','#D5546EFF',
+      '#E76F5AFF','#F58C46FF','#FDAD32FF','#FCD225FF','#F0F921FF'
+    ],
+    opacity: 0.8
+  };
+  options = {...default_options, ...options};
+
+  // Reverse labels and palette
+  let rev_labels = [...options.legend_breaks].reverse();
+  let rev_palette = [...options.fill_palette].reverse();
+
+  let div = L.DomUtil.create("div", options.className);
+  // Optionally add title
+  if(options.legend_title){
+    let title_div = L.DomUtil.create("div");
+    title_div.innerHTML = "<strong>" + options.legend_title + "</strong>";
+    title_div.style.marginBottom = "3px";
+    div.appendChild(title_div);
+  }
+
+  // Formatting variables
+  let vMargin = 8; // If 1st tick mark starts at top of gradient, how
+  // many extra px are needed for the top half of the
+  // 1st label? (ditto for last tick mark/label)
+  let tickWidth = 4;     // How wide should tick marks be, in px?
+  let labelPadding = 2;  // How much distance to reserve for tick mark?
+  // Each bin is `singleBinHeight` high. How tall is the gradient?
+  let totalHeight = 151;
+  // The distance between tick marks, in px
+  let singleBinHeight = (totalHeight - 1) / (rev_labels.length - 1);
+
+  let gradSpan = document.createElement("span");
+  Object.assign(gradSpan.style, {
+    background: `linear-gradient(${rev_palette})`,
+    opacity: options.opacity,
+    height: totalHeight + "px",
+    width: "30px",
+    display: "block",
+    marginTop: vMargin + "px"
+  });
+
+  let leftDiv = document.createElement("div");
+  leftDiv.style.float = "left";
+  let rightDiv = document.createElement("div");
+  rightDiv.style.float = "left";
+
+  leftDiv.appendChild(gradSpan);
+  div.appendChild(leftDiv);
+  div.appendChild(rightDiv);
+  div.appendChild(document.createElement("br"));
+
+  // Have to attach the div to the body at this early point, so that the
+  // svg text getComputedTextLength() actually works, below.
+  document.body.appendChild(div);
+
+  let ns = "http://www.w3.org/2000/svg";
+  let svg = document.createElementNS(ns, "svg");
+  rightDiv.appendChild(svg);
+  let g = document.createElementNS(ns, "g");
+  g.setAttribute("transform", "translate(0, " + vMargin + ")");
+  svg.appendChild(g);
+
+  // max label width needed to set width of svg, and right-justify text
+  let maxLblWidth = 0;
+
+  // Create tick marks and labels
+  rev_labels.forEach((label, i) => {
+    let y = i*singleBinHeight + 0.5;
+
+    let thisLabel = document.createElementNS(ns, "text");
+    thisLabel.textContent = label;
+    thisLabel.setAttribute("y", y);
+    thisLabel.setAttribute("dx", tickWidth + labelPadding);
+    thisLabel.setAttribute("dy", "0.5ex");
+    g.appendChild(thisLabel);
+    maxLblWidth = Math.max(maxLblWidth, thisLabel.getComputedTextLength());
+
+    let thisTick = document.createElementNS(ns, "line");
+    thisTick.setAttribute("x1", 0);
+    thisTick.setAttribute("x2", tickWidth);
+    thisTick.setAttribute("y1", y);
+    thisTick.setAttribute("y2", y);
+    thisTick.setAttribute("stroke-width", 1);
+    thisTick.setAttribute("stroke", "black");
+    g.appendChild(thisTick);
+  });
+
+  // Final size for <svg>
+  Object.assign(svg.style, {
+    width: (maxLblWidth + labelPadding) + "px",
+    height: totalHeight + vMargin*2 + "px"
+  });
+
+  // Return the legend div
+  return div;
+}
+
+
 // Helper function to create a GeoJSON layer for Leaflet
-function new_geojson(data, ind_suffix = ''){
+function new_geojson(data, options){
+  const default_options = {
+    ind_suffix: '',
+    type: 'polygons'
+  };
+  options = {...default_options, ...options};
+  let style_fun = {}
+  if(options.type === 'polygons'){
+    let fill_column = options.use_col + options.ind_suffix;
+    let fill_scale = (chroma
+      .scale(options.fill_palette)
+      .domain([options.lower, options.upper])
+    );
+    style_fun = (feature) => {
+      let feature_color = fill_scale(feature.properties[fill_column]);
+      return {
+        color: feature_color,
+        fillColor: feature_color,
+        fillOpacity: 0.85
+      };
+    }
+  }
   return L
-    .geoJSON(data)
+    .geoJSON(data, {style: style_fun})
     .bindTooltip(function(layer){
-      return poly_tooltop(layer, ind_suffix);
+      return poly_tooltop(layer, options.ind_suffix);
     });
 }
+
 
 function new_tile(url, options){
   const default_options = {
     attribution: '',
     subdomains: 'abcd',
-    maxzoom: 20
-  }
-  return L.tileLayer(url, options || default_options);
+    maxzoom: 20,
+    pane: 'tilePane'
+  };
+  options = {...default_options, ...options};
+  return L.tileLayer(url, options);
 }
 
-// Template to create a leaflet map
+// Template to create a leaflet map ----------------------------------------------------->
+
 // Assumes the div with id map already exists
-function create_map(id, bounds) {
+function create_map(id, bounds, options) {
+  const default_options = {
+    use_col: 'prev15to49_mean',
+    lower: 0.0,
+    upper: 0.15,
+    fill_palette: [
+      '#0D0887FF','#3E049CFF','#6300A7FF','#8707A6FF','#A62098FF','#C03A83FF','#D5546EFF',
+      '#E76F5AFF','#F58C46FF','#FDAD32FF','#FCD225FF','#F0F921FF'
+    ],
+    legend_breaks: ['0%', '5%', '10%', '15%+'],
+    legend_title: 'Estimated<br/>HIV Prevalence',
+    pop_cutoff_low: 50,
+    pop_cutoff_high: 200
+  };
+  options = {...default_options, ...options};
+
   // Create map
   const map = L.map(id);
   const bounds_keys = Object.keys(bounds);
+
   // Add base layers
   new_tile(
     'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
@@ -105,10 +253,11 @@ function create_map(id, bounds) {
   ).addTo(map);
   new_tile(
     'https://tiles.stadiamaps.com/tiles/stamen_toner_lines/{z}/{x}/{y}{r}.png',
-    {subdomains: ''}
+    {subdomains: '', pane: 'shadowPane'}
   ).addTo(map);
   new_tile(
-    'https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png'
+    'https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png',
+    {pane: 'shadowPane'}
   ).addTo(map);
 
   // Add district boundaries
@@ -122,43 +271,45 @@ function create_map(id, bounds) {
   map.fitBounds(district_layer.getBounds());
   // Create all toggleable layers
   var base_layers = {};
-  base_layers['High resolution'] = new_geojson(bounds.h3).addTo(map);
-  base_layers['Group village head'] = new_geojson(bounds.gvh, ind_suffix = '_gvh');
-  base_layers['Traditional authority'] = new_geojson(bounds.ta);
+  base_layers['High resolution'] = new_geojson(bounds.h3, options).addTo(map);
+  base_layers['Group village head'] = new_geojson(bounds.gvh, {...options, ind_suffix: '_gvh'});
+  base_layers['Traditional authority'] = new_geojson(bounds.ta, options);
   base_layers['Facility catchment'] = new_geojson(
     bounds.closest_facilities,
-    ind_suffix = '_gcf'
+    {...options, ind_suffix: '_gcf'}
   );
   // Add layers that may not exist: survey facilities
   if(bounds_keys.includes('survey_facilities')){
     base_layers['Survey facilities'] = new_geojson(
       bounds.survey_facilities,
-      ind_suffix = '_scf'
+      {...options, ind_suffix: '_scf'}
     );
   }
   // Add optional layers:
   var optional_layers = {};
-  optional_layers['Health facility locations'] = new_geojson(bounds.facility_points);
+  optional_layers['Health facility locations'] = new_geojson(
+    bounds.facility_points,
+    {type: 'points'}
+  );
   if(bounds_keys.includes('dropped_facility_points')){
     optional_layers['<i>(Excluded health facilities)</i>'] = new_geojson(
-      bounds.dropped_facility_points
+      bounds.dropped_facility_points,
+      {type: 'points'}
     );
   }
-  optional_layers['Facility catchment<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;boundaries'] = L.geoJSON(
+  const catch_title = 'Facility catchment<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;boundaries';
+  optional_layers[catch_title] = L.geoJSON(
     bounds.closest_facilities,
-    {
-      style: {
-        opacity: 0.85,
-        color: "#0000FF",
-        dashArray: "5, 10",
-        fillOpacity: 0
-      }
-    }
+    {style: {opacity: 0.85, color: "#0000FF", dashArray: "5, 10", fillOpacity: 0}}
   );
-  const layerControl = L.control.layers(
-    base_layers, optional_layers, {
-      collapsed: false,
-    }
-  );
+  const layerControl = L.control.layers(base_layers, optional_layers, {collapsed: false});
   layerControl.addTo(map);
+
+  // Add legend
+  var legend = L.control({position: 'bottomright'});
+  legend.onAdd = function(_){
+    const div = prepare_legend(options);
+    return div;
+  };
+  legend.addTo(map);
 }
